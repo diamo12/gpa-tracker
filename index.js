5cb228c046724a2976b933c1c61037de40e1c80d
Brandon Fields <44679104+diamo12@users.noreply.github.com>
2019-10-30 09:46:19 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function initListeners(){
 function loadCurrentGrades() {
   $('#GPA_Table').DataTable({
     ajax: {
-      url: "https://github.com/diamo12/gpa-tracker/blob/master/grades.txt",
+      url: "https://github.com/diamo12/gpa-tracker/grades.txt",
       dataSrc: '',
       type: "GET",
       headers: {
@@ -43,7 +43,7 @@ function deleteEntry() {
 }
 
 function getGrades() {
-  var fileUrl = "grades.txt";
+  var fileUrl = "https://github.com/diamo12/gpa-tracker/grades.txt";
 
   return $.ajax({
     url: fileUrl,
